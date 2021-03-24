4f3c62cc9de439ed6e8c29b141a85038341f6001
Roberto Sanchez <rodsanchezdu@unal.edu.co>
2021-03-24 08:10:57 -0500

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -14,13 +14,15 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { FormsModule } from "@angular/forms";
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule, 
@@ -34,7 +36,8 @@ import { FormsModule } from "@angular/forms";
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
 })
 export class PagesModule { }
